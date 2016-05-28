d20b7b0bcbe1f4af4bafb66e086060cb3b11e14e
Chirou-Cat <garyoakheart@gmail.com>
2016-05-28 14:31:10 +0100

    dem batch skillz

--- gitRemind.bat
+++ gitRemind.bat
@@ -1,12 +1,37 @@
+@echo off
+rem Test Builds
+If "%1"=="--test" goto GitTest
 rem Define "chk"
 SET chk=%ERRORLEVEL%
 
-rem Format Drive
-E:
-cd \Ironic-SRC
+:AutoUpdate
+rem Autoupdate times
+If %time%==10:00:00.00 goto GitUpdate10
+If %time%==14:00:00.00 goto GitUpdate14
+if %time%==20:00:00.00 goto GitUpdate20
+rem Reminder: Not DoS friendly...
+goto AutoUpdate
 
+:GitUpdate10
+rem Dir should be fine.
 rem Update it
 git pull
-
 rem Tell me when its done
-msg %Username% Pull Passed Exit Code %chk%
+msg %Username% Autoupdater: 10 AM Pull Passed
+goto AutoUpdate
+
+:GitUpdate14
+git pull
+msg %Username% Autoupdater: 2 PM Pull Passed
+goto AutoUpdate
+
+:GitUpdate20
+git pull
+msg %Username% Autoupdater: 8 PM Pull Passed
+goto AutoUpdate
+
+rem Dev reasons only...
+:GitTest
+git pull
+msg %Username% Git TEST: Git Test Passed
+goto AutoUpdate
